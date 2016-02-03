a25c21fe7fd1f160a707e5da55e7e4f5462acdcf
Lucilla Chalmer <chalmer.lucilla@gmail.com>
2016-02-03 12:12:50 -0800

    added debounce

--- app/web/app/js/dao/app_dao.js
+++ app/web/app/js/dao/app_dao.js
@@ -1,12 +1,24 @@
 import AjaxUtils from 'ajax_utils';
 import querystring from 'querystring';
+import Promise from 'bluebird'
 
 module.exports = {
-  searchCraigslist(query) {
-    let queryObj = {
-      search: query
-    };
-
-    return AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`);
-  }
+  searchCraigslist: (() => {
+    let timeout;
+    return (query) => {
+      return new Promise((resolve, reject) => {      
+        let queryObj = {
+          search: query
+        };
+        if (timeout) {
+          clearTimeout(timeout)
+        }
+        timeout = setTimeout(() => {
+          AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`).then((query) => {
+            resolve(query);
+          })
+        }, 250);
+      })
+    }
+  })()
 }
